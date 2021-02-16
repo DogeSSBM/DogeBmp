1cfda7b1027adcde09a0b293f0e6f0015d3c3423
DogeSSBM <dogessbm@gmail.com>
2021-02-15 23:23:51 -0500

    Automatic commit.

---
+++ Types.h
@@ -0,0 +1,57 @@
+#pragma once
+typedef FILE			File;
+typedef time_t			Time;
+typedef unsigned int		Ticks;
+
+typedef long int			lint;
+typedef unsigned char		uchar;
+typedef unsigned int		uint;
+typedef unsigned long		ul;
+typedef unsigned long long	ull;
+
+typedef uint8_t			u8;
+typedef uint16_t			u16;
+typedef uint32_t			u32;
+typedef uint64_t			u64;
+
+typedef int8_t			i8;
+typedef int16_t			i16;
+typedef int32_t			i32;
+typedef int64_t			i64;
+
+#define I8MIN			INT8_MIN
+#define I8MAX			INT8_MAX
+#define I16MIN			INT16_MIN
+#define I16MAX			INT16_MAX
+#define I32MIN			INT32_MIN
+#define I32MAX			INT32_MAX
+#define I64MIN			INT64_MIN
+#define I64MAX			INT64_MAX
+
+#define U8MAX			UINT8_MAX
+#define U16MAX			UINT16_MAX
+#define U32MAX			UINT32_MAX
+#define U64MAX			UINT64_MAX
+
+#define ABS(n)			((n)<0? -(n):(n))
+#define ZFLOOR(n)			((n)<0?    0:(n))
+
+typedef SDL_Event			Event;
+typedef SDL_Rect 			Rect;
+typedef SDL_Color 		Color;
+typedef SDL_Surface 		Img;
+typedef SDL_Keycode 		Key;
+typedef SDL_BlendMode		BlendMode;
+typedef SDL_GameController	Controller;
+#define BLEND_NONE		SDL_BLENDMODE_NONE
+#define BLEND_ALPHA		SDL_BLENDMODE_BLEND
+#define BLEND_ADD			SDL_BLENDMODE_ADD
+#define BLEND_MOD			SDL_BLENDMODE_MOD
+
+#define PI				M_PI
+
+typedef enum{
+	FULLSCREEN	=		SDL_WINDOW_FULLSCREEN,
+	BORDERLESS	=		SDL_WINDOW_FULLSCREEN_DESKTOP,
+	WINDOWED	=		0
+}WindowMode;
